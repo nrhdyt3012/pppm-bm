a65564779fd2ca9d0aec590c6e547b7360de5c24
nrhdyt3012 <dayats354313@gmail.com>
2026-05-17 21:49:35 +0700

    set env

--- src/app/beranda/page.tsx
+++ src/app/beranda/page.tsx
@@ -3,7 +3,7 @@ import Homepage from "./_components/homepage";
 export const metadata = {
   title: "PAUD ABA 1 Buduran | Beranda",
   icons: {
-    icon: "./favicon.ico",
+    icon: "/favicon.ico",
   },
 };
 
